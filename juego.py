ba6dd76be2d25969cf30c87d69ebe5a556fd710f
Hugo Ruscitti <hugoruscitti@gmail.com>
2022-03-25 22:23:39 -0300

    Agregando imagenes.

---
+++ juego.py
@@ -0,0 +1,34 @@
+import pyglet
+
+window = pyglet.window.Window(style=pyglet.window.Window.WINDOW_STYLE_DEFAULT)
+
+titulo = pyglet.text.Label('Salvando La Cena',
+                          font_name='Arial',
+                          font_size=36,
+                          x=window.width//2, 
+                          y=window.height//1.2,
+                          anchor_x='center', 
+                          anchor_y='center')
+
+empezar = pyglet.text.Label('Cualquier tecla\npara comenzar',
+                          font_name='Arial',
+                          multiline=True,
+                          font_size=20,
+                          x=window.width//1.9, 
+                          y=window.height//2.5,
+                          width=200,
+                          anchor_x='center', 
+                          anchor_y='center')
+
+jugador_1 = pyglet.resource.image("imagenes/jugador1.png")
+jugador_2 = pyglet.resource.image("imagenes/jugador2.png")
+
+@window.event
+def on_draw():
+    window.clear()
+    titulo.draw()
+    empezar.draw()
+    jugador_1.blit(0, 75)
+    jugador_2.blit(400, 75)
+
+pyglet.app.run()
